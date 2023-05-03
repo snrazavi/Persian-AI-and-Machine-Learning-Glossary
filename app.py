e3e3c21499eed7520013c543547735e11aad4282
snrazavi <razavinaser@gmail.com>
2023-05-03 16:20:29 +0100

    Using stars to show ratings

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import difflib
 from dotenv import load_dotenv
+from helpers import generate_star_rating
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 load_dotenv()
@@ -33,28 +34,6 @@ def save_dictionary(dictionary):
         yaml.dump(dictionary, file, allow_unicode=True)
 
 
-def search_dictionary(term):
-    """Search the dictionary for a term"""
-    term = term.lower()
-    return dictionary.get(term)
-    # # search for a term in a list of dictionaries
-    # for entry in dictionary:
-    #     if entry['english'].lower() == term:
-    #         return entry['persian']
-    # return None
-    # translations = dictionary.get(term.lower())
-    # if translations is None:
-    #     # find the closest match to the term
-    #     closest_match = difflib.get_close_matches(term, dictionary.keys(), n=1, cutoff=0.6)
-    #     if closest_match:
-    #         closest_match = closest_match[0]
-    #         return render_template("index.html", translations=translations, term=similar_term, original_term=term)
-    #     else:
-    #         return render_template("index.html", not_found=True, term=term)
-    # else:
-    #     return render_template("index.html", translations=translations, term=term)
-
-
 dictionary = load_dictionary()
 
 @app.route("/", methods=["GET", "POST"])
@@ -75,14 +54,16 @@ def index():
                     translations = [translation for translation in translations 
                                     if translation.get('approved', True)]
                     return render_template(
-                        "index.html", translations=translations, 
-                        term=similar_term, original_term=term)
+                        "index.html", generate_star_rating=generate_star_rating,
+                        translations=translations, term=similar_term, original_term=term)
                 else:
                     return render_template("index.html", not_found=True, term=term)
             else:
                 translations = [translation for translation in translations 
                                 if translation.get('approved', True)]
-                return render_template("index.html", translations=translations, term=term)
+                return render_template(
+                    "index.html", generate_star_rating=generate_star_rating, 
+                    translations=translations, term=term)
         elif "new-english-term" in request.form and "new-persian-translation" in request.form:
             english_term = request.form.get("new-english-term")
             persian_translation = request.form.get("new-persian-translation")
@@ -104,29 +85,33 @@ def index():
 @app.route("/rate_translation", methods=["POST"])
 def rate_translation():
     """Rate a translation"""
-    english_term = request.form.get("english-term")
-    persian_translation = request.form.get("persian-translation")
-    rating = int(request.form.get("rating"))
+    english_term = request.form.get("term")
+    translation_index = request.form.get("translation_index")
 
-    translations = dictionary.get(english_term.lower())
-    if translations:
-        for translation_entry in translations:
-            if translation_entry["persian"] == persian_translation:
-                if "rating_no" not in translation_entry:
-                    translation_entry["rating_no"] = 0
-                translation_entry["rating_no"] += 1
-                # compute the new rating by a weighted average
-                rating = (translation_entry["rating_no"] * translation_entry["rating"] + rating) / (translation_entry["rating_no"] + 1)
-                # round the rating to the nearest 0.5#
-                rating = round(rating * 2) / 2
-                translation_entry["rating"] = rating
-                print(f"Rating for {english_term} - {persian_translation} updated to {rating}")
-                # sort the translations based on the rating
-                translations = sorted(translations, key=lambda x: x['rating'], reverse=True)
-                # update the dictionary
-                dictionary[english_term.lower()] = translations
-                break
+    try:
+
+        rating = int(request.form.get("new_rating"))    
+        translations = dictionary.get(english_term.lower())
+        translation_entry = translations[int(translation_index)]
+        if "rating_no" not in translation_entry:
+            translation_entry["rating_no"] = 0
+        translation_entry["rating_no"] += 1
+
+        # compute the new rating by a weighted average
+        rating = (translation_entry["rating_no"] * translation_entry["rating"] + rating) / (translation_entry["rating_no"] + 1)
+        # round the rating to the nearest 0.5
+        rating = round(rating * 2) / 2
+        translation_entry["rating"] = rating
+
+        print(f"New rating: {rating}")
+        # sort the translations based on the rating
+        translations = sorted(translations, key=lambda x: x['rating'], reverse=True)
+        # update the dictionary
+        dictionary[english_term.lower()] = translations
         save_dictionary(dictionary)
+        flash("Thank you for rating the translation!")
+    except:
+        flash("Please select a rating!")
 
     return redirect(url_for("index"))
 
