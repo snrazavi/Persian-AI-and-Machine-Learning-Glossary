2c9b42ad2026b35a41aa67237a3e514d6ea083dc
snrazavi <razavinaser@gmail.com>
2023-05-03 17:38:22 +0100

    remove old files

--- glossary.py
+++ glossary.py
@@ -1,111 +1,72 @@
-""" This module contains a class for storing and manipulating a glossary."""
+"""This module contains functions for loading, saving, searching, and adding to the dictionary."""
 import yaml
 
-
 class Glossary:
-    """
-    A class for storing and manipulating a glossary.
-    """
-    def __init__(self, glossary_file: str ='Glossary.yaml') -> None:
-        """
-        Initialize the glossary
-        :param glossary_file: YAML file containing the glossary
-        """
-        self.glossary_file = glossary_file
-        self.glossary = self.read_glossary()
-        self._deduplicate_glossary()
+    """A class for loading, saving, searching, and adding to the dictionary"""
+    def __init__(self, dictionary_file="Glossary_for_ratings.yaml") -> None:
+        """Initialize the dictionary"""
+        self.dictionary_file = dictionary_file
+        self._load_dictionary()
 
-    def __len__(self) -> int:
-        """
-        Return the number of terms in the glossary
-        :return: number of terms in the glossary
-        """
-        return len(self.glossary)
+    def _load_dictionary(self):
+        """Load the dictionary from a YAML file"""
+        with open(self.dictionary_file, "r", encoding="utf-8") as file:
+            dictionary = yaml.safe_load(file)
 
-    @staticmethod
-    def sort_dicts_by_key(dicts: list[dict], key: str, reverse: bool=False) -> list[dict]:
-        """
-        Sort a list of dictionaries by a key
-        :param dicts: list of dictionaries
-        :param key: key to sort by
-        :param reverse: whether to sort in reverse order
-        :return: sorted list of dictionaries
-        """
-        return sorted(dicts, key=lambda x: x[key], reverse=reverse)
+        # sort the dictionary for each term based on the rating
+        for term in dictionary:
+            dictionary[term] = sorted(dictionary[term], key=lambda x: x['rating'], reverse=True)
 
-    def _deduplicate_glossary(self) -> None:
-        """
-        Remove duplicate terms from the glossary
-        :return: None
-        """
-        duplicate_found = False
-        deduplicated_glossary = []
-        for term in self.glossary:
-            if term not in deduplicated_glossary:
-                deduplicated_glossary.append(term)
-            else:
-                print(f"Duplicate term: {term['english']}")
-                duplicate_found = True
-        self.glossary = deduplicated_glossary
-        if duplicate_found:
-            print("Duplicate terms found in the glossary. Rewriting the glossary file...")
-            self.write_glossary(self.glossary, self.glossary_file)
+        self.dictionary = dictionary
 
-    def read_glossary(self) -> list[dict]:
-        """
-        Read the glossary from a YAML file
-        :return: glossary
-        """
-        with open(self.glossary_file, 'r', encoding='utf-8') as file:
-            glossary = yaml.load(file, Loader=yaml.FullLoader)
-        return glossary
 
-    def write_glossary(self, glossary: list[dict], glossary_file: str) -> None:
-        """
-        Write the glossary to a YAML file
-        :param glossary: glossary
-        :param glossary_file: YAML file to write the glossary to
-        :return: None
-        """
-        with open(glossary_file, 'w', encoding='utf-8') as file:
-            yaml.dump(glossary, file, allow_unicode=True)
+    def save_dictionary(self):
+        """Save the dictionary to a YAML file"""
+        with open(self.dictionary_file, "w", encoding="utf-8") as file:
+            yaml.dump(self.dictionary, file, allow_unicode=True)
 
-    def sort_glossary(self, language: str='en') -> list[dict]:
-        """
-        Sort the glossary terms in alphabetical order reading from a YAML file
-        and write the sorted glossary to a new YAML file
-        :param language: language of the glossary
-        :return: None
-        """
-        # sort the glossary terms in alphabetical order based on the language
-        if language == 'en':
-            sorted_glossary = self.sort_dicts_by_key(self.glossary, 'english')
-        elif language == 'fa':
-            sorted_glossary = self.sort_dicts_by_key(self.glossary, 'persian')
+
+    def search_dictionary(self, term):
+        """Search the dictionary for a term"""
+        term = term.lower()
+        translations = self.dictionary.get(term)
+        if translations is None:
+            return None
+        return [translation for translation in translations if translation.get("approved", True)]
+
+
+    def add_translation(self, english_term, persian_translation):
+        """Add a new translation to the dictionary"""
+        english_term = english_term.lower()
+        new_entry = {"persian": persian_translation, "rating": 0, "approved": False}
+
+        for entry in self.dictionary.get(english_term, []):
+            if entry["persian"] == persian_translation:
+                return False
+
+        if english_term in self.dictionary:
+            self.dictionary[english_term].append(new_entry)
         else:
-            raise ValueError('language should be either "en" or "fa"')
+            self.dictionary[english_term] = [new_entry]
 
-        return sorted_glossary
-    
-    def convert_glossary_format_for_ratings(self) -> list[dict]:
-        """
-        Convert the glossary format to the format required for ratings
-        :return: glossary in the format required for ratings
-        """
-        glossary = []
-        for entry in self.glossary:
-            english = entry['english']
-            persian = entry['persian']
-            # split persian terms by semicolon
-            persian_terms = persian.split('؛')
-            translations = [{'persian': persian_term.strip(), 'rating': 0} for persian_term in persian_terms]
-            glossary.append({'english': english, 'translations': translations})
-
-        # save the glossary in the format required for ratings
-        self.write_glossary(glossary, 'Glossary_for_ratings.yaml')
-        return glossary
+        self.save_dictionary()
+        return True
 
 
-if __name__ == '__main__':
-    enfa_glossary = Glossary('Glossary.yaml')
-    print(f"Number of terms in the glossary: {len(enfa_glossary)}")
+    def update_rating(self, english_term, translation_index, new_rating):
+        """Update the rating of a translation"""
+        english_term = english_term.lower()
+        translations = self.dictionary.get(english_term)
+        translation_entry = translations[int(translation_index)]
+
+        if "rating_no" not in translation_entry:
+            translation_entry["rating_no"] = 0
+        translation_entry["rating_no"] += 1
+
+        rating = (translation_entry["rating_no"] * translation_entry["rating"] + new_rating) / (translation_entry["rating_no"] + 1)
+        rating = round(rating * 2) / 2
+        translation_entry["rating"] = rating
+
+        translations = sorted(translations, key=lambda x: x["rating"], reverse=True)
+        self.dictionary[english_term] = translations
+        self.save_dictionary()
