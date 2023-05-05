e70bc0072ea79625d1f2e7593d75dca8d4fcec6d
snrazavi <razavinaser@gmail.com>
2023-05-05 13:55:53 +0100

    bugfix

--- views/main.py
+++ views/main.py
@@ -16,66 +16,99 @@ from helpers import generate_star_rating
 main = Blueprint("main", __name__)
 
 
+def search_glossary_term(glossary_term: str):
+    """ Search the database for the given term.
+    If the term is found, return the translations.
+    If the term is not found, search for the closest match and return its translations.
+    If no match is found, return None.
+
+    :param glossary_term: the term to search for
+    :type glossary_term: str
+    :return: the translations of the term
+    """
+    term_entry = GlossaryTerm.query.filter_by(english_term=glossary_term).first()
+    if term_entry:
+        return term_entry.translations
+
+    # search for the closest match to the term
+    all_terms = [glossary_term.english_term for glossary_term in GlossaryTerm.query.all()]
+    closest_match = difflib.get_close_matches(glossary_term, all_terms, n=1, cutoff=0.6)
+    if closest_match:
+        similar_term = closest_match[0]
+        translations = GlossaryTerm.query.filter_by(english_term=similar_term).first().translations
+        return translations, similar_term
+
+    # no match found
+    return None
+
+
+def submit_new_translation(english_term: str, persian_translation: str):
+    """Submit a new translation to the database.
+
+    :param english_term: the English term
+    :type english_term: str
+    :param persian_translation: the Persian translation
+    :type persian_translation: str
+    """
+    term_entry = GlossaryTerm.query.filter_by(english_term=english_term).first()
+    translation_entry = Translation.query.filter_by(persian_translation=persian_translation).first()
+
+    if translation_entry:
+        return False
+
+    if term_entry:
+        glossary_term_id = term_entry.id
+    else:
+        new_glossary_term = GlossaryTerm(english_term=english_term)
+        db.session.add(new_glossary_term)
+        db.session.commit()
+        db.session.flush() # to get the id of the new glossary term
+        glossary_term_id = new_glossary_term.id
+
+    new_translation = Translation(
+        persian_translation=persian_translation, glossary_term_id=glossary_term_id)
+    db.session.add(new_translation)
+    db.session.commit()
+    return True
+
+
 @main.route("/", methods=["GET", "POST"])
 def index():
     """Home page"""
     if request.method == "POST":
         if "search-term" in request.form:
-            term = request.form.get("search-term")
-            term = term.strip().lower()
-            term_entry = GlossaryTerm.query.filter_by(english_term=term).first()
-            if term_entry:
-                translations = term_entry.translations
-                return render_template(
-                    "index.html", generate_star_rating=generate_star_rating, 
-                    translations=translations, term=term)
-            
-            # search for the closest match to the term
-            all_terms = [glossary_term.english_term for glossary_term in GlossaryTerm.query.all()]
-            closest_match = difflib.get_close_matches(term, all_terms, n=1, cutoff=0.6)
-            if closest_match:
-                similar_term = closest_match[0]
-                translations = GlossaryTerm.query.filter_by(english_term=similar_term).first().translations
+            term = request.form.get("search-term").strip().lower()
+            result = search_glossary_term(term)
+
+            if result:
+                if isinstance(result, tuple):
+                    translations, similar_term = result
+                    return render_template(
+                        "index.html", generate_star_rating=generate_star_rating,
+                        translations=translations, term=similar_term, original_term=term)
+
+                translations = result
                 return render_template(
                     "index.html", generate_star_rating=generate_star_rating,
-                    translations=translations, term=similar_term, original_term=term)
-            
-            # no match found
+                    translations=translations, term=term)
+
             return render_template("index.html", not_found=True, term=term)
-                
+
         if "new-english-term" in request.form and "new-persian-translation" in request.form:
-            english_term = request.form.get("new-english-term")
-            persian_translation = request.form.get("new-persian-translation")
+            english_term = request.form.get("new-english-term").strip().lower()
+            persian_translation = request.form.get("new-persian-translation").strip()
 
             # cherck the translation and the term are not empty
             if not english_term or not persian_translation:
                 flash("Please enter a valid term and translation!")
-                return redirect(url_for("index"))
+                return redirect(url_for("main.index"))
 
-            term_entry = GlossaryTerm.query.filter_by(english_term=english_term).first()
-
-            # check if the translation already exists in the dictionary
-            translation_entry = Translation.query.filter_by(persian_translation=persian_translation).first()
-            if translation_entry:
-                flash("This translation already exists in the dictionary!")
-                return redirect(url_for("index"))
-            else:
-                if term_entry:
-                    glossary_term_id = term_entry.id
-                else:
-                    new_glossary_term = GlossaryTerm(english_term=english_term)
-                    db.session.add(new_glossary_term)
-                    db.session.commit()
-                    db.session.flush() # to get the id of the new glossary term
-                    glossary_term_id = new_glossary_term.id
-
-                new_translation = Translation(
-                    persian_translation=persian_translation, glossary_term_id=glossary_term_id)
-                db.session.add(new_translation)
-                db.session.commit()
+            if submit_new_translation(english_term, persian_translation):
                 flash("Thank you for your contribution!\
                       Your suggestion will be added to the dictionary after approval.")
-                return redirect(url_for("index"))
+            else:
+                flash("This translation already exists in the dictionary!")
+            return redirect(url_for("main.index"))
 
     return render_template("index.html")
 
@@ -83,7 +116,7 @@ def index():
 @main.route("/rate_translation", methods=["POST"])
 def rate_translation():
     """Rate a translation"""
-    english_term = request.form.get("term")
+    english_term = request.form.get("term").strip().lower()
     translation_index = request.form.get("translation_index")
 
     term_entry = GlossaryTerm.query.filter_by(english_term=english_term).first()
@@ -101,9 +134,8 @@ def rate_translation():
             # round the rating to the nearest 0.5
             translation.rating = round(translation.rating * 2) / 2
             db.session.commit()
-            # glossary.update_rating(english_term, translation_index, new_rating)
             flash("Thank you for your rating!")
         else:
             flash("Please select a rating in range 1-5!")
 
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
