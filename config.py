64fc95cb2879b2e22a4568f2db2435f6a30b9aca
snrazavi <razavinaser@gmail.com>
2023-05-05 13:05:52 +0100

    update config

--- config.py
+++ config.py
@@ -1,28 +1,16 @@
 """This file contains the configuration for the Flask app."""
 import os
-# from dataclasses import dataclass
+from dataclasses import dataclass
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-# load RDS credentials from environment variables
-RDS_USERNAME = os.environ.get('RDS_USERNAME')
-RDS_PASSWORD = os.environ.get('RDS_PASSWORD')
-RDS_ENDPOINT = os.environ.get('RDS_ENDPOINT')
-RDS_PORT = 3306  # os.environ.get('RDS_PORT')
-RDS_DB_NAME = os.environ.get('RDS_DB_NAME')
-
-# read access key and secret access key from environment variables
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
-
-# @dataclass
+@dataclass
 class Config:
     """A class for the Flask app configuration"""
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{RDS_USERNAME}:{RDS_PASSWORD}@{RDS_ENDPOINT}:{RDS_PORT}/{RDS_DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.environ.get('RDS_USERNAME')}:{os.environ.get('RDS_PASSWORD')}@{os.environ.get('RDS_ENDPOINT')}:3306/{os.environ.get('RDS_DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
